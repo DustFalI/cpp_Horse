091872e15bf087682abfd9a612903d82678569a9
DustFalI <1540500607@qq.com>
2025-03-20 11:02:51 +0800

    Update Horse.cpp

--- Horse.cpp
+++ Horse.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m, x, y, land[410][410] = {0};
-int dx[8] = {-2, -2, -1, 1, 2, 2, -1, 1}, dy[8] = {-1, 1, 2, 2, -1, 1, -2, -2};
+int dx[8] = {-2, -2, -1, 1, 2, 2, -1, 1}, dy[8] = {-1, 1, 2, 2, -1, 1, -2, -2};//偏移变量
 struct point
 {
-	int lx, ly, step;
+	int lx, ly, step;//每个位置单独存储自己的步数，方式同一步数的多个位置因for循环而错乱
 };
 queue<point> q;
 int main()
 {
-	scanf("%d%d%d%d", &n, &m, &x, &y);
+	scanf("%d%d%d%d", &n, &m, &x, &y);//养成好习惯，每个文件用同一种输入输出
 	for(int i = 1; i <= n; i++)
 	{
 		for(int j = 1; j <= m; j++)
 		{
-			land[i][j] = -1;
+			land[i][j] = -1;//初始化为-1
 		}
 	}
-	land[x][y] = 0;
-	q.push(point{x,y,0});
-	int step = 1;
+	land[x][y] = 0;//起点标记为0步
+	q.push(point{x,y,0});//起点入队
+	int step = 1;//步数初始化为1
 	while(!q.empty())
 	{
 		point p = q.front();
@@ -28,10 +28,10 @@ int main()
 		{
 			int nx = p.lx + dx[i], ny = p.ly + dy[i];
 			if(nx < 1 ||  ny < 1 || nx > n+1 || ny > m+1) continue;
-			if(land[nx][ny] == -1)
+			if(land[nx][ny] == -1)//-1代表没有被标记
 			{
-				land[nx][ny] = p.step+1;
-				q.push(point{nx,ny,p.step+1});
+				land[nx][ny] = p.step+1;//
+				q.push(point{nx,ny,p.step+1});//
 			}
 		}
 	}
@@ -39,7 +39,7 @@ int main()
 	{
 		for(int j = 1; j <= m; j++)
 		{
-			printf("%-5d", land[i][j]);
+			printf("%-5d", land[i][j]);//-5表示每个元素占5个位置
 		}
 		printf("\n");
 	}
